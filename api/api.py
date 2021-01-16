b7eec9142d950a4a2aa6277aed70c837633a4e37
Tabish Rashidi <tabish.rashidi@gmail.com>
2021-01-16 17:59:25 -0500

    Adding endpoints, rebase with master

--- api/api.py
+++ api/api.py
@@ -1,18 +1,47 @@
-import flask 
+import flask
 
 from flask import request, jsonify 
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True; 
-
-@app.route('/', methods=['GET'])
-def home(): 
-	return "<h1>This site is a prototype API</h1>"
-
-
-@app.route('/api/v1/resources', methods=['GET'])
-def firstGet(): 
+app.config["DEBUG"] = True
 
 
 
-app.run()
+# Display contents on login
+@app.route('/api/v1/contacts', methods=['GET'])
+def home():
+    return "<h1>This site is a prototype API</h1>"
+
+
+
+# Adds a contact
+@app.route('/api/v1/contact', methods=['POST'])
+def addContact(): 
+	pass
+
+# Delete contact 
+@app.route('/api/v1/contact' methods=['DELETE'])
+def delContact():
+	pass
+
+
+# Create a room 
+@app.route('/api/v1/user/room_id', methods=['POST'])
+def createRoom(): 
+	pass
+
+
+# Join a room 
+@app.route('/api/v1/user/room_id', methods=['GET'])
+def joinRoom():
+
+
+@app.route('/api/v1/login', methods=['POST'])
+def login():  
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
