3d0464c08b54342cf0090c7c740879f5675b5176
Kevin Jin <kevin.jin@vanderbilt.edu>
2021-01-16 20:45:06 -0700

    nice

--- frontend/src/ContactCard.js
+++ frontend/src/ContactCard.js
@@ -4,17 +4,30 @@ import img from "./images/family.jpg"
 
 const ContactCardBackground = styled.div`
   background-image: url(${img});
-  font-size: 32px;
-  color: white;
+  background-size: cover;
+  height: 20em;
+  width: 30em;
+  border-radius: 1.5em;
+`;
+
+const ContactCardTextShadow = styled.div`
+  background-color: rgba(0, 0, 0, 0.2);
+`;
+
+const ContactCardText = styled.p`
+  font-size: 2em;
+  opacity: 1.0;
+  padding: 0.4em;
+  text-align: center;
+  font-weight: bold;
 `;
 
 function ContactCard(){
-
   return(
     <ContactCardBackground>
-      <div>
-        <p>Family</p>
-      </div>
+      <ContactCardTextShadow>
+        <ContactCardText>Family</ContactCardText>
+      </ContactCardTextShadow>
     </ContactCardBackground>
   );
 }
