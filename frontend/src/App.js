eda9fb95789e1ae8e2770bab66f2995407e64efd
Kevin Jin <kevin.jin@vanderbilt.edu>
2021-01-16 21:30:57 -0700

    make cards work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from "styled-components";
 import ContactCard from "./ContactCard";
 import Sidebar from './Test'
 const items = [
@@ -7,6 +8,12 @@ const items = [
   {name: 'profile', label: 'Profile'}, 
   {name: 'setting', label: 'Setting'}, 
 ]
+
+const CardWrapper = styled.div`
+  display: inline-block;
+  width: 100%;
+  margin-left: 2em;
+`;
 
 function App(){
 
@@ -17,11 +24,15 @@ function App(){
   console.log(counter);
   return(
     <div>
-      <ContactCard/>
       <Sidebar items = {items} />
-      <h1>Hello React </h1>
-      <button> hello </button>
-      </div>
+      <CardWrapper>
+        <ContactCard name="Family" />
+        <ContactCard name="Friends" />
+        <ContactCard name="Stephen" />
+        <ContactCard name="Sabrina" />
+        <ContactCard name="Mom and Dad" />
+      </CardWrapper>
+    </div>
   );
 }
 export default App;
--- frontend/src/ContactCard.js
+++ frontend/src/ContactCard.js
@@ -2,33 +2,51 @@ import React from 'react';
 import styled from "styled-components";
 import img from "./images/family.jpg"
 
+const ContactCardTextShadow = styled.div`
+  background-color: rgba(255, 255, 255, 0.4);
+  width: 100%;
+`;
+
+const ContactCardText = styled.p`
+  font-size: 2em;
+  opacity: 1.0;
+  padding: 0.5em;
+  margin: 0em;
+  text-align: center;
+  font-weight: bold;
+`;
+
 const ContactCardBackground = styled.div`
   background-image: url(${img});
   background-size: cover;
   height: 20em;
   width: 30em;
   border-radius: 1.5em;
+  border : 0.4em solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  &:hover ${ContactCardTextShadow} {
+    background-color: rgba(181, 178, 25, 0.4);
+  }
 `;
 
-const ContactCardTextShadow = styled.div`
-  background-color: rgba(0, 0, 0, 0.2);
-`;
+const ContactCardWrapper = styled.div`
+  display: inline-block;
+  margin-right: 5em;
+  margin-bottom: 5em;
+`
 
-const ContactCardText = styled.p`
-  font-size: 2em;
-  opacity: 1.0;
-  padding: 0.4em;
-  text-align: center;
-  font-weight: bold;
-`;
-
-function ContactCard(){
+function ContactCard(props){
   return(
-    <ContactCardBackground>
-      <ContactCardTextShadow>
-        <ContactCardText>Family</ContactCardText>
-      </ContactCardTextShadow>
-    </ContactCardBackground>
+    <ContactCardWrapper>
+      <ContactCardBackground>
+        <ContactCardTextShadow>
+          <ContactCardText>{props.name}</ContactCardText>
+        </ContactCardTextShadow>
+      </ContactCardBackground>
+    </ContactCardWrapper>
   );
 }
 export default ContactCard;
