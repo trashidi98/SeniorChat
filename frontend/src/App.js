45d281e4cb83961a3c292601ddfaba6b6ca9131e
Yuxin Zhang <yuxinzhang@Yuxins-MacBook-Pro.local>
2021-01-16 21:31:26 -0800

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ContactCard from "./ContactCard";
 import Sidebar from './Test'
 const items = [
-  {name: 'home', label:'Home'},
-  {name: 'searching', label:'Searching'},
-  {name: 'profile', label: 'Profile'}, 
-  {name: 'setting', label: 'Setting'}, 
+  {name: 'home', label:'Home', image: "https://cdn1.iconfinder.com/data/icons/free-98-icons/32/call-512.png"},
+  {name: 'searching', label:'Searching',image: "https://static.thenounproject.com/png/105498-200.png"},
+  {name: 'profile', label: 'Profile',image: "https://icons-for-free.com/iconfiles/png/512/avatar+human+man+profile+icon-1320085876716628234.png"}, 
+  {name: 'setting', label: 'Setting',image: "https://simpleicon.com/wp-content/uploads/setting2.png"}, 
 ]
 
 function App(){
