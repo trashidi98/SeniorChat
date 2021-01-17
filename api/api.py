682f015c4d5e38e4334e5e331cc084f526b2840a
Saif Masoud <saifemasoud@gmail.com>
2021-01-17 06:08:47 +0300

    Database In-progress (My brain is blanking but I am about to get it done.)

--- api/api.py
+++ api/api.py
@@ -1,34 +1,87 @@
 import flask
 import sqlite3
-
 from flask import request, jsonify 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config["DEBUG"] = True
+db = SQLAlchemy(app)
 
+# MODELS:
+class User(db.Model):
+    id_ = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    roomId = db.Column(db.Integer)
 
+    @property
+    def serialize(self):
+       """Return object data in easily serializable format"""
+       return {
+           'id': self.id_,
+           'username': self.username,
+           'email': self.email,
+
+       }
+
+    def __repr__(self):
+        return '<User %r %r>' % (self.username, self.email)
+
+class ContactMapping(db.Model):
+    id_ = db.Column(db.Integer, primary_key=True)
+    first_user_id = db.Column(db.Integer)
+    second_user_id = db.Column(db.Integer)
+
+    def __repr__(self):
+        return ('<ContactMapping> %r -> %r' % (self.first_user_id, self.second_user_id))
+    
 
 # Display contents on login
 @app.route('/api/v1/contacts', methods=['GET'])
 def displayContacts():
-    return "<h1>This site is a prototype API</h1>"
+    user_id = int(request.headers.get('user_id'))
+    contact_ids = [mapping.second_user_id for mapping in ContactMapping.query.filter_by(first_user_id=user_id).all()]
+    contacts: List[User] = [User.query.filter_by(id_=contact_id).first() for contact_id in contact_ids]
+    return jsonify(json_list=[contact.serialize for contact in contacts])
+
+
 
 
 # Adds a contact
 @app.route('/api/v1/contact', methods=['POST'])
-def addContact(): 
-    args = request.args
+def addContact():
+    breakpoint()
+    user_id = int(request.headers.get('user_id'))
+    friend_email = request.json.get('friend_email')
+    try:
+        friend_user: User = User.query.filter_by(email=friend_email).all()[0]
+        mapping = ContactMapping(first_user_id=user_id, second_user_id=friend_user.id_)
+        db.session.add(mapping)
+        db.session.commit()
+    except IndexError: # if there are no users with given email
+        pass
+    return jsonify({})
+
 
 # Delete contact 
 @app.route('/api/v1/contact', methods=['DELETE'])
 def delContact():
-	args = request.args
+    user_id = int(request.headers.get('user_id'))
+    friend_email = request.json.get('friend_user_name')
+    friend_user: User = User.query.filter_by(email=friend_email).all()[0]
+    ContactMapping(first_user_id=user_id, second_user_id=friend_user.id_).delete()
+    db.session.commit()
+    return jsonify({})
+
 
 
 # Create a room 
 @app.route('/api/v1/user/room_id', methods=['POST'])
 def createRoom(): 
-	args = request.args
+    user_id = int(request.headers.get('user_id'))
+    room_id = request.json.get('room_id')
 
 
 # Join a room 
@@ -44,19 +97,6 @@ def login():
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>This site is a prototype API</h1>"
-
-
-conn = sqlite3.connect('data.db')
-cursor = conn.cursor()
-
-cursor.execute("""CREATE TABLE users (
-    id int, 
-    email varchar(255),
-    Name varchar(255)
-)""")
-
-cursor.execute("""CREATE TABLE friendmappings""")
-
 
 
 if __name__ == "__main__":
