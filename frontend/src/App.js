ce591101a23e46dd4d4819d262215d85000fc4b9
Yuxin Zhang <yuxinzhang@Yuxins-MacBook-Pro.local>
2021-01-16 17:38:06 -0800

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App(){
+
+  const sayHello = () =>{
+    console.log("hello"); 
+  }; 
+  const counter = 0; 
+  console.log(counter);
+  return(
+    <div>
+      <h1>Hello React </h1>
+      <button> hello </button>
+      </div>
+  );
+}
+export default App; 
