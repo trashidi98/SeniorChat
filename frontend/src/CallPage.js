abe582a3d6b1ad56d3d14a0fb73feb4cd89c24a1
Kevin Jin <kevin.jin@vanderbilt.edu>
2021-01-17 03:23:32 -0700

    videos

--- frontend/src/CallPage.js
+++ frontend/src/CallPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from "styled-components";
+import { connect, createLocalVideoTrack } from "twilio-video";
 
+const TEST_TOKEN = "hidden"
 
 const CallWrapper = styled.div`
   padding: 2em;
@@ -20,13 +22,63 @@ const CallWindow = styled.div`
   background-color: #dbdbdb;
   width: 70em;
   height: 50em;
+  margin: 3em;
 `
 
+const watchSelf = () => {
+  createLocalVideoTrack().then(track => {
+    const localMediaContainer = document.getElementById('local-media');
+    localMediaContainer.appendChild(track.attach());
+  });
+}
+
+const joinRoom = (roomName) => {
+  connect(TEST_TOKEN, { audio: true, name: roomName, video: { width: 640 } }).then(room => {
+    console.log(`Successfully joined a Room: ${room}`);
+
+    // Get video for already connected participants
+    room.participants.forEach(participant => {
+      participant.tracks.forEach(publication => {
+        if (publication.track) {
+          document.getElementById('remote-media-div').appendChild(publication.track.attach());
+        }
+      });
+    
+     participant.on('trackSubscribed', track => {
+        document.getElementById('remote-media-div').appendChild(track.attach());
+      });
+    });
+
+
+    room.on('participantConnected', participant => {
+      console.log(`A remote Participant connected: ${participant}`);
+
+      // Get video for newly connected participants
+      participant.tracks.forEach(publication => {
+        if (publication.isSubscribed) {
+          const track = publication.track;
+          document.getElementById('remote-media-div').appendChild(track.attach());
+        }
+      });
+    
+      participant.on('trackSubscribed', track => {
+        document.getElementById('remote-media-div').appendChild(track.attach());
+      });
+
+    });
+  }, error => {
+    console.error(`Unable to connect to Room: ${error.message}`);
+  });
+};
+
 function CallPage(props){
+  watchSelf();
+  joinRoom("what");
   return(
     <CallWrapper>
       <CallTitle>Calling {props.callGroup}</CallTitle>
-      <CallWindow/>
+      <CallWindow id="local-media" />
+      <CallWindow id="remote-media-div" height="50em" />
     </CallWrapper>
   );
 }
