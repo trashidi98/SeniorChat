a1c8ff3599c7b3374d5a710477c6a76adc75636b
Kevin Jin <kevin.jin@vanderbilt.edu>
2021-01-17 01:42:11 -0700

    share code lmao

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import styled from "styled-components";
 import ContactCard from "./ContactCard";
 import Sidebar from './Sidebar';
-
-
-
+import Header from './Header'
 
 
 const items = [
   {name: 'home', label:'Home', image: "https://cdn1.iconfinder.com/data/icons/free-98-icons/32/call-512.png"},
-  {name: 'searching', label:'Searching',image: "https://static.thenounproject.com/png/105498-200.png"},
+  {name: 'searching', label:'Add Friends',image: "https://static.thenounproject.com/png/105498-200.png"},
   {name: 'profile', label: 'Profile',image: "https://icons-for-free.com/iconfiles/png/512/avatar+human+man+profile+icon-1320085876716628234.png"}, 
   {name: 'setting', label: 'Setting',image: "https://simpleicon.com/wp-content/uploads/setting2.png"}, 
 ]
@@ -20,28 +18,42 @@ const CardWrapper = styled.div`
   margin-left: 2em;
 `;
 
+const AppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction; row;
+`
 
+const SidebarPlaceholder = styled.div`
+  width: 20em;
+  height: 1px;
+`
 
 function App(){
-
-
   const sayHello = () =>{
     console.log("hello"); 
   }; 
   const counter = 0; 
   console.log(counter);
   return(
-    <div>
-      <Sidebar items = {items} />
-      <CardWrapper>
-        <ContactCard name="Family" />
-        <ContactCard name="Friends" />
-        <ContactCard name="Stephen" />
-        <ContactCard name="Sabrina" />
-        <ContactCard name="Mom and Dad" />
-      </CardWrapper>
-    </div>
+    <AppWrapper>
+      <Header/>
+      <ContentWrapper>
+        <CardWrapper>
+          <ContactCard name="Family" />
+          <ContactCard name="Friends" />
+          <ContactCard name="Stephen" />
+          <ContactCard name="Sabrina" />
+          <ContactCard name="Mom and Dad" />
+        </CardWrapper>
+        <SidebarPlaceholder/>
+        <Sidebar items = {items} />
+      </ContentWrapper>
+    </AppWrapper>
   );
 }
 export default App;
--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -1,27 +1,57 @@
 import React from 'react';
 import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-const styles = theme => ({
-  listItemText:{
-    fontSize:'1.7em',//Insert your required size
+import styled from "styled-components";
+
+
+const SidebarWrapper = styled.div`
+  border-left: 1px solid rgba(0,0,0);
+  width: 20em;
+  position: fixed;
+  right: 0px;
+  top: 0px;
+  height: 100%;
+`;
+
+const SidebarItem = styled.div`
+  font-size: 1.7em;
+  padding: 0.5em;
+  margin: 0em;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  &:hover {
+    background-color: #eaf24e;
   }
-});
+`;
+
+const HeaderItem = styled.div`
+  font-size: 2em;
+  padding: .7em;
+  padding-bottom: 1em;
+  margin: 0em;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-weight: bold;
+`;
+
+const SidebarLabel = styled.div`
+  padding-left: 0.2em;
+`;
+
 function Sidebar({ items }) {
   return (
-    <div className = "sidebar">
-    <List disablePadding dense>
-      {items.map(({ label, name,image, ...rest }) => (
-        <ListItem key={name} button {...rest}> 
-        <img src = {image} width="40" height="40"></img>
-          <ListItemText>{label}</ListItemText>
-          
-        </ListItem>
-      ))}
-    </List>
-
-
-    </div>
+    <SidebarWrapper>
+      <List disablePadding dense>
+        <HeaderItem>SeniorChat</HeaderItem>
+        {items.map(({ label, name,image, ...rest }) => (
+          <SidebarItem key={name} button {...rest}> 
+          <img src = {image} width="40" height="40"></img>
+          <SidebarLabel>{label}</SidebarLabel>  
+          </SidebarItem>
+        ))}
+      </List>
+    </SidebarWrapper>
   )
 }
 
